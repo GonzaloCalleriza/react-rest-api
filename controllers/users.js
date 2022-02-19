ad392c0ce60b9bd05fad1eeb5c35f402eafdcbb7
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-19 12:31:27 +0100

    Finished user REST API

---
+++ controllers/users.js
@@ -0,0 +1,55 @@
+import {v4 as uuidv4} from 'uuid';
+
+let users = [];
+
+export const createUser = (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`User ${user.firstName} ${user.lastName}, who is ${user.age}, has been added to the database`)
+};
+
+export const getUsers = (req, res) => {
+    res.send(users);
+};
+
+export const getUser = (req, res) => {
+
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+};
+
+export const deleteUser = (req, res) => {
+
+    const { id, firstName, lastName } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User ${firstName} ${lastName} with id ${id} has been removed`);
+};
+
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if(firstName){
+        user.firstName = firstName;
+    }
+
+    if(lastName){
+        user.lastName = lastName;
+    }
+
+    if(age){
+        user.age = age;
+    }
+
+    res.send(`USer with id: ${id} has been updated`)
+
+}
